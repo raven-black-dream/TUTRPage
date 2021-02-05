176892254d72cf67ffbb67a7e77295f5b6e0a71c
raven-black-dream <evan.harley21@gmail.com>
2021-02-04 19:14:47 -0800

    Prepping for deployment

--- TUTRPage/urls.py
+++ TUTRPage/urls.py
@@ -23,10 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += [
-        path(r'__debug__/', include(debug_toolbar.urls)),
-    ]
