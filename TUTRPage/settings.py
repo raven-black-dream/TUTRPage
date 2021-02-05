4206b994492699f452887e52c414507b51b1714c
raven-black-dream <evan.harley21@gmail.com>
2021-02-04 19:18:16 -0800

    Fixing collectstatic

--- TUTRPage/settings.py
+++ TUTRPage/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
